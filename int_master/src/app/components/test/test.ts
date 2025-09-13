2a57070ca8b33b59ad4dc380f96d621c1f17f07c
filbertik <aton2019zvit@gmail.com>
2025-09-13 22:54:08 +0300

    add tesr button

--- int_master/src/app/components/test/test.ts
+++ int_master/src/app/components/test/test.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-test',
   imports: [],
   templateUrl: './test.html',
-  styleUrl: './test.scss'
+  styleUrl: './test.scss',
 })
 export class Test {
+  title = 'Це  інтерполяція !';
+  firstName = 'Viktor';
+  lastName = 'Kukharskyi';
 
+  isEnabled: boolean = true;
+
+  getFullName() {
+    return `I'm  ${this.firstName}  ${this.lastName}`;
+  }
 }
