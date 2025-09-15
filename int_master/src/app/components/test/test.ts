3a62cf298af2d765b8283edeea0a09c45943cbb2
Filbertik <aton2019zvit@gmail.com>
2025-09-15 23:48:54 +0300

    inline styles

--- int_master/src/app/components/test/test.ts
+++ int_master/src/app/components/test/test.ts
@@ -1,19 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './test.html',
-  styleUrl: './test.scss',
+  styleUrls: ['./test.scss'], //  styleUrls
 })
 export class Test {
-  title = 'Це  інтерполяція !';
+  title = 'Це інтерполяція !';
   firstName = 'Viktor';
   lastName = 'Kukharskyi';
 
-  isEnabled: boolean = true;
+  isEnabled: boolean = false; //switch of button
+  isActive: boolean = false;
+  isDisabled: boolean = true;
 
   getFullName() {
-    return `I'm  ${this.firstName}  ${this.lastName}`;
+    return `I'm ${this.firstName} ${this.lastName}`;
   }
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-test',
+//   imports: [],
+//   templateUrl: './test.html',
+//   styleUrl: './test.scss',
+// })
+// export class Test {
+//   title = 'Це  інтерполяція !';
+//   firstName = 'Viktor';
+//   lastName = 'Kukharskyi';
+
+//   isEnabled: boolean = true;
+
+//   isActive: boolean = true;
+//   isDisabled: boolean = false;
+
+//   getFullName() {
+//     return `I'm  ${this.firstName}  ${this.lastName}`;
+//   }
+// }
